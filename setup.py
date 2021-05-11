806534fad5a85807f99b5635cc2d46ce24bbdfdb
Simone Montali <sim.montali@gmail.com>
2021-05-11 11:04:32 +0200

    Setup and probs printing

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='flatland-temperatureobservation',
-    version='0.0.1',    
-    install_requires=["numpy", "flatland-rl", "scipy"]
+    version='0.0.1',
+    install_requires=["numpy", "flatland-rl", "scipy"],
+    packages=find_packages()
 )
--- temperature_observation/utils.py
+++ temperature_observation/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def normalize_observation(observation):
     """Normalizes a TemperatureObservation to a flattened array
 
@@ -8,3 +11,22 @@ def normalize_observation(observation):
         np.ndarray: Flattened observation
     """
     return observation.flatten()
+
+
+def format_action_prob(action_probs):
+    """Generates a probability string 
+
+    Args:
+        action_probs (np.ndarray): action probabilities array
+
+    Returns:
+        string: string of probabilities to print out
+    """
+    action_probs = np.round(action_probs, 3)
+    actions = ["↻", "←", "↑", "→", "◼"]
+
+    buffer = ""
+    for action, action_prob in zip(actions, action_probs):
+        buffer += action + " " + "{:.3f}".format(action_prob) + " "
+
+    return buffer
